f3f98333288d645415f8cec777de22061a0d93ca
noahsperling <sperling19@up.edu>
2016-08-31 16:28:58 -0700

    updated method and fixed an oversight

--- app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -15,9 +15,11 @@ public class PrimeTester {
      */
     public static boolean isPrime(long n) {
         // for now, return a random result
-        if (n == 2 || n == 3 || n == 5 || n == 7 || n == 13) {
+        if (n == 0 || n == 1) {
 
             return false;
+        } else if (n == 2 || n == 3 || n == 5 || n == 7 || n == 13){
+            return true;
         }
         for (int i = 2; i < n; i++) {
             if (n % i == 0) {
